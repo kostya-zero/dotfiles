bddf7e34fc189d5d02365eaf640126195f43ff91
Konstantin Zhigaylo <zero@kostyazero.com>
2026-04-24 12:43:43 +0300

    feat(neovim): use standard white for foreground

--- dot_config/nvim/lua/plugins/colorscheme.lua
+++ dot_config/nvim/lua/plugins/colorscheme.lua
@@ -9,5 +9,8 @@ return {
             dark = "jellybeans_mono",
             light = "jellybeans_mono_light",
         },
+        on_colors = function(c)
+            c.foreground = "#c7c7c7"
+        end,
     },
 }
