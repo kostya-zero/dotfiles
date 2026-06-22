3765b92823df6c26512dbadf7ad1172bf0f0abc6
Konstantin Zhigaylo <zero@kostyazero.com>
2026-06-22 09:09:10 +0300

    feat(fish): remove mise

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -15,5 +15,7 @@ alias l "eza --icons --long"
 set --export BUN_INSTALL "$HOME/.bun"
 set --export PATH $BUN_INSTALL/bin $PATH
 
-# mise
-mise activate fish | source
+export PATH="$HOME/.local/bin:$PATH"
+
+# zoxide
+zoxide init fish | source
