f3622c39cab841b5e19a26e30cd6bd3033109f17
Konstantin Zhigaylo <kostyazero@proton.me>
2026-09-26 18:18:11 +0300

    feat(neovim): remove git plugins

--- dot_config/nvim/lua/plugins/git.lua
+++
@@ -1,9 +0,0 @@
-return {
-    {
-        "NeogitOrg/neogit",
-        cmd = "Neogit",
-        keys = {
-            { "<leader>gG", "<cmd>Neogit<cr>", desc = "Show Neogit UI" },
-        },
-    },
-}
